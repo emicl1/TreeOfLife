979f24fc26eb804c5788ecb95a2767caf961c65f
Alex Olivier Michaud <michaal4@fel.cvut.cz>
2024-04-11 17:45:33 +0200

    added better diagram and project decription

--- TreeOfLife/core/src/tree/of/life/logic/Character.java
+++ TreeOfLife/core/src/tree/of/life/logic/Character.java
@@ -5,11 +5,20 @@ public class Character {
     private int health;
     private int attackDamage;
     private boolean isAlive;
+    private int speed;
+    private int x;
+    private int y;
+    public Items[] items;
 
-    public Character(String name, int health, int attackDamage, boolean isAlive){
+    public Character(String name, int health, int attackDamage, boolean isAlive, int speed, int x, int y, Items[] items){
         this.name = name;
         this.health = health;
         this.attackDamage = attackDamage;
+        this.isAlive = isAlive;
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+        this.items = items;
     }
 
     public String getName(){
@@ -35,6 +44,59 @@ public class Character {
     public void setAttackDamage(int attackDamage){
         this.attackDamage = attackDamage;
     }
+
+    public void setIsAlive(boolean isAlive){
+        this.isAlive = isAlive;
+    }
+
+    public boolean getIsAlive(){
+        return isAlive;
+    }
+
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void takeDamage(int damage){
+        health -= damage;
+        if(health <= 0){
+            isAlive = false;
+        }
+    }
+
+    public void attack(Character target){
+        target.takeDamage(attackDamage);
+    }
+
+    public void heal(int heal){
+        health += heal;
+    }
+
+    public void move(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
 
 
 }
--- TreeOfLife/core/src/tree/of/life/logic/Enemies.java
+++ TreeOfLife/core/src/tree/of/life/logic/Enemies.java
@@ -1,10 +1,12 @@
 package tree.of.life.logic;
 
+import com.badlogic.gdx.graphics.g3d.particles.ParticleSorter;
+
 public class Enemies extends NPC{
 
 
-    public Enemies(String name, int health, int attackDamage, boolean isAlive) {
-        super(name, health, attackDamage, isAlive);
+    public Enemies(String name, int health, int attackDamage, boolean isAlive, int speed, int x, int y, boolean isHostile, boolean hasInformation, Items[] items, String[] quests) {
+        super(name, 100, attackDamage, isAlive, speed, x, y, true, hasInformation, items, quests);
     }
 
     public int Attack(){
